5322f3ee60735bf96a9e4d24b5004befbe8684b3
ACHA <mac@Q.local>
2025-07-18 04:23:37 -0700

    Shopping View Layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,18 @@ import AdminDashboard from "./pages/admin-view/dashboard";
 import AdminProducts from "./pages/admin-view/products";
 import AdminOrders from "./pages/admin-view/orders";
 import AdminFeatures from "./pages/admin-view/features";
-
+import ShoppingLayout from "./components/ui/shopping-view/layout";
+import ShoppingHome from "./pages/shopping-view/home";
+import ShoppingListing from "./pages/shopping-view/listing";
+import ShoppingCheckout from "./pages/shopping-view/checkout";
+import ShoppingAccount from "./pages/shopping-view/account";
+import NotFound from "./pages/not-found";
 
 function App() {
   return (
     <div className="flex flex-col overflow-hidden bg-white">
+      {/* COMMON COMPONENT */}
+      <h1>Hearder Component</h1>
       <Routes>
           <Route path="/auth" element={<AuthLayout/>}>
               <Route path="login" element={<AuthLogin/>}/>
@@ -23,6 +30,13 @@ function App() {
               <Route path="orders" element={<AdminOrders/>}/>
               <Route path="features" element={<AdminFeatures/>}/>
           </Route>
+          <Route path="/shop" element={<ShoppingLayout/>}>
+              <Route path="home" element={<ShoppingHome/>}/>
+              <Route path="listing" element={<ShoppingListing/>}/>
+              <Route path="checkout" element={<ShoppingCheckout/>}/>
+              <Route path="account" element={<ShoppingAccount/>}/>
+          </Route>
+          <Route path="*" element={<NotFound/>}/>
       </Routes>
     </div>
   )
