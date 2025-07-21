6129961b1522d8c28be8cda9a9bc341a13a6a6d0
ACHA <mac@Q.local>
2025-07-21 11:59:23 -0700

    User sign up & email notification

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,3 +1,24 @@
 const User = require("../Models/userModel");
+const catchAsync = require("../Utils/catchAsync");
+const Email = require("../Utils/email");
 
-exports.userSignUp
+exports.userSignUp = catchAsync( async (req, res, next) => {
+    const newUser = await User.create(req.body)
+
+    const OTPToken = await createOTP(newUser);
+    await newUser.save({ validateBeforeSave: false });
+
+    try{
+        await new Email(newUser, OTPToken).sendOTPEmail();
+
+        res.status(200).json({
+            status:"success",
+            message: "OTP sent to email"
+        })
+    }catch(err){
+        newUser.otpToken = undefined;
+        newUser.otpExpires = undefined;
+        newUser.save({ validateBeforeSave: false });
+        console.log(err);
+    }
+});
---
+++ server/Utils/appFeatures.js
@@ -0,0 +1,11 @@
+const crypto = require("crypto");
+const jwt = require("sonwebtoken");
+const bcrypt = require("bcryptjs");
+
+exports.createOTP = async ( operator ) => {
+    const OTP = crypto.randomBytes(2).toString("hex");
+    operator.otpToken = crypto.createHash("sha256").update(OTP).digest("hex");
+
+    operator.otpExpires = Date.now() + 10 * 60 * 60;
+    return OTP
+};
