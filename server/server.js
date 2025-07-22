855b11595abc49387c1676fb272053b5c454f4c7
ACHA <mac@Q.local>
2025-07-21 17:28:21 -0700

    user registration endpoint completion

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ mongoose.connect(DB)
 })
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App is running on port ${port}...`)
 });
 
