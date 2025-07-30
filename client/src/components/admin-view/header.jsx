13fce9568d96c31379c3012c036ee799ac84eedb
Acha-Goodness <acharious1668@gmail.com>
2025-07-30 03:46:50 -0700

    admin sidebar configuration

--- client/src/components/admin-view/header.jsx
+++ client/src/components/admin-view/header.jsx
@@ -5,13 +5,13 @@ import { GiHamburgerMenu } from "react-icons/gi";
 
 const AdminHeader = () => {
   return (
-    <header className='flex items-center justify-between px-4 py-3 bg-background border-bottom'>
+    <header className='flex items-center justify-between px-4 py-3 bg-background border-b'>
       <Button className="lg:hidden sm:block">
         <GiHamburgerMenu />
         <span className='sr-only'>Toggle Menu</span>
       </Button>
       <div className='flex flex-1 justify-end'>
-        <Button>
+        <Button className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium">
           <FaPowerOff />
           Logout
         </Button>
--- client/src/components/admin-view/sidebar.jsx
+++ client/src/components/admin-view/sidebar.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { Fragment } from 'react';
+import { RiAdminFill } from "react-icons/ri";
+import { useNavigate } from 'react-router-dom';
+
 
 const AdminSideBar = () => {
+
+  const navigate = useNavigate();
   return (
-    <div>admin sidebar</div>
+    <Fragment>
+      <aside className='hidden w-64 flex-col border-r bg-[purple] p-6 lg:flex'>
+        <div onClick={() => navigate("/admin/dashboard")} className='flex cursor-pointer items-center gap-2'>
+          <RiAdminFill size={30}/>
+          <h1 className='text-xl font-extrabold'>Admin Panel</h1>
+        </div>
+      </aside>
+    </Fragment>
   )
 }
 
