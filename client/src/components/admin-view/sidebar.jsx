c30f9188ba7b154b0f339872483fa886583f4a8c
Acha-Goodness <acharious1668@gmail.com>
2025-07-30 04:15:30 -0700

    sidebar menu update

--- client/src/components/admin-view/sidebar.jsx
+++ client/src/components/admin-view/sidebar.jsx
@@ -1,15 +1,40 @@
 import React, { Fragment } from 'react';
 import { RiAdminFill } from "react-icons/ri";
 import { useNavigate } from 'react-router-dom';
-import { adminSidebarMenuItems } from '../config';
+import { TbLayoutDashboardFilled } from "react-icons/tb";
+import { MdOutlineProductionQuantityLimits } from "react-icons/md";
+import { FaCubesStacked } from "react-icons/fa6";
+
+    const adminSidebarMenuItems = [
+        {
+            id: "dashboard",
+            label: "Dashboard",
+            path: "/admin/dashboard",
+            icon: <TbLayoutDashboardFilled />
+        },
+        {
+            id: "products",
+            label: "Products",
+            path: "/admin/products",
+            icon: <MdOutlineProductionQuantityLimits />
+        },
+        {
+            id: "orders",
+            label: "Orders",
+            path: "/admin/orders",
+            icon: <FaCubesStacked />
+        }
+    ]
 
 const MenuItems = () => {
+  const navigate = useNavigate();
   return (
     <nav className='mt-8 flex-col flex gap-2'>
         {
           adminSidebarMenuItems.map(menuItem => 
-          <div className='flex items-center gap-2 rounded-md px-3 py-2'>
-
+          <div key={menuItem.id} onClick={() => navigate(menuItem.path)} className='flex items-center gap-2 rounded-md px-3 py-2'>
+              {menuItem.icon}
+              <span>{menuItem.label}</span>
           </div>
           )
         }
@@ -28,6 +53,7 @@ const AdminSideBar = () => {
           <RiAdminFill size={30}/>
           <h1 className='text-xl font-extrabold'>Admin Panel</h1>
         </div>
+        <MenuItems/>
       </aside>
     </Fragment>
   )
--- client/src/components/config/index.js
+++ client/src/components/config/index.js
@@ -108,20 +108,3 @@
         }
     ]
 
-    export const adminSidebarMenuItems = [
-        {
-            id: "dashboard",
-            label: "Dashboard",
-            path: "/admin/dashboard"
-        },
-        {
-            id: "products",
-            label: "Products",
-            path: "/admin/products"
-        },
-        {
-            id: "orders",
-            label: "Orders",
-            path: "/admin/orders"
-        }
-    ]
