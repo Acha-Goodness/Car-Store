f3ce13aab40c73eeb9c14f79f2f04a5273184812
Acha-Goodness <acharious1668@gmail.com>
2025-07-30 03:31:06 -0700

    admin dashboard navigation configuration

--- client/src/components/admin-view/header.jsx
+++ client/src/components/admin-view/header.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Button } from '../ui/button';
+import { FaPowerOff } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const AdminHeader = () => {
   return (
-    <div>admin header</div>
+    <header className='flex items-center justify-between px-4 py-3 bg-background border-bottom'>
+      <Button className="lg:hidden sm:block">
+        <GiHamburgerMenu />
+        <span className='sr-only'>Toggle Menu</span>
+      </Button>
+      <div className='flex flex-1 justify-end'>
+        <Button>
+          <FaPowerOff />
+          Logout
+        </Button>
+      </div>
+    </header>
   )
 }
 
--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -7,7 +7,7 @@ const AdminLayout = () => {
         <div className="flex min-h-screen w-full">
             {/* admin sidebar */}
             <AdminSideBar/>
-            <div className="flex fkex-1 flex-col">
+            <div className="flex flex-1 flex-col">
                 {/* admin header */}
                 <AdminHeader/>
                 <main className="flex-1 flex bg-black p-4 md:p-6">
