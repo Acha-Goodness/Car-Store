5179849d4eccb572a849abcce09fca82899314c5
Acha-Goodness <acharious1668@gmail.com>
2025-08-01 01:47:53 -0700

    edit product asyncthunk creation

--- client/src/store/admin/product-slice/index.js
+++ client/src/store/admin/product-slice/index.js
@@ -20,6 +20,15 @@ export const fetchAllProducts = createAsyncThunk("/products/fetchAllProducts", a
     return result?.data;
 });
 
+export const editProducts = createAsyncThunk("/products/editProducts", async ({ id, formData }) => {
+    const result = await axios.post(`http://localhost:3000/api/v1/admin/products/edit/:${id}`, formData, {
+        hearders: {
+            "Content-Type" : "application/json"
+        },
+    })
+    return result?.data;
+});
+
 const AdminProductsSlice = createSlice({
     name: "adminPoducts",
     initialState,
