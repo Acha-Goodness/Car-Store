f87ebf785f6ed2f4e7b4362393ca972f03eeebd2
Acha-Goodness <acharious1668@gmail.com>
2025-08-01 07:35:15 -0700

    server side delete product endpoint creation

--- server/Routes/Admin/productRoutes.js
+++ server/Routes/Admin/productRoutes.js
@@ -5,9 +5,9 @@ const { upload } = require("../../Helpers/cloudinary");
 const router = express.Router();
 
 router.post("/upload-image", upload.single("my_file"), handleImageUpload);
-router.post("/add", addProduct)
-router.put("/edit/:id", editProduct)
-router.post("/delete/:id", deleteProduct)
+router.post("/add", addProduct);
+router.put("/edit/:id", editProduct);
+router.delete("/delete/:id", deleteProduct);
 router.get("/get", fetchProducts);
 
 module.exports = router;
