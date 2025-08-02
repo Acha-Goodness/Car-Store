239bf17f12441ef0297fa24c85709d1016e6894e
Acha-Goodness <acharious1668@gmail.com>
2025-08-02 13:57:18 -0700

    handleLogout function created

--- client/src/components/admin-view/header.jsx
+++ client/src/components/admin-view/header.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import { Button } from '../ui/button';
 import { FaPowerOff } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { useDispatch } from 'react-redux';
+import { logout } from '@/store/auth-slice';
 
 const AdminHeader = ({setOpen}) => {
+
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   return (
     <header className='flex items-center justify-between px-4 py-3 bg-background border-b'>
       <Button onClick={() => setOpen(true)} className="lg:hidden sm:block">
@@ -11,7 +20,7 @@ const AdminHeader = ({setOpen}) => {
         <span className='sr-only'>Toggle Menu</span>
       </Button>
       <div className='flex flex-1 justify-end'>
-        <Button className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium text-[red]">
+        <Button onClick={handleLogout} className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium text-[red]">
           <FaPowerOff/>
           Logout
         </Button>
