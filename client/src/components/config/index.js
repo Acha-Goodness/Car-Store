f4b0c1293aca28631d1b3d0e1ae195d2624bbb66
Acha-Goodness <acharious1668@gmail.com>
2025-08-02 15:02:33 -0700

    created filter option for listing page

--- client/src/components/config/index.js
+++ client/src/components/config/index.js
@@ -170,3 +170,7 @@
         }
     ];
 
+    export const filterOptions = [
+        {id: "nike", }
+    ]
+
---
+++ client/src/components/shopping-view/filter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProductFilter = () => {
+  return (
+    <div className='bg-[grey] rounded-lg shadow-sm'>
+        <div className='p-4 border-b'>
+            <h2 className='text-lg font-semibold'>Filters</h2>
+        </div>
+        <div className='p-4 space-y-4'>
+
+        </div>
+    </div>
+  )
+}
+
+export default ProductFilter;
