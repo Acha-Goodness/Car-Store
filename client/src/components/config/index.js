fe1ae78d2cf272c7d2ec82cc3953daa900f5dcd5
Acha-Goodness <acharious1668@gmail.com>
2025-08-02 15:11:26 -0700

    Created category for filter options

--- client/src/components/config/index.js
+++ client/src/components/config/index.js
@@ -171,13 +171,20 @@
     ];
 
     export const filterOptions = {
-    brand: [
-        {id: "nike", label: "Nike" },
-        {id: "addidas", label: "Addidas"},
-        {id: "puma", label: "Puma"},
-        {id: "levi", label: "Levi's"},
-        {id: "zara", label: "Zara"},
-        {id: "h&m", label: "H&M"}
-    ],
+        category: [
+            {id: "men", label: "Men" },
+            {id: "women", label: "Women"},
+            {id: "kids", label: "Kids"},
+            {id: "accessories", label: "Accessories"},
+            {id: "footwear", label: "Footwear"},
+        ],
+        brand: [
+            {id: "nike", label: "Nike" },
+            {id: "addidas", label: "Addidas"},
+            {id: "puma", label: "Puma"},
+            {id: "levi", label: "Levi's"},
+            {id: "zara", label: "Zara"},
+            {id: "h&m", label: "H&M"}
+        ],
     }
 
