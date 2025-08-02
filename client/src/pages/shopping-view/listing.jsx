2cdbc361fea4f4d0e941d5310204fa5eef21c4fb
Acha-Goodness <acharious1668@gmail.com>
2025-08-02 16:50:18 -0700

    Product sort menu option dropdown design

--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -1,27 +1,39 @@
 import ProductFilter from '@/components/shopping-view/filter';
 import React from 'react'
-import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { Button } from '@/components/ui/button';
 import { LuArrowUpDown } from "react-icons/lu";
+import { sortOptions } from '@/components/config';
 
 const ShoppingListing = () => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6'>
       <ProductFilter/>
       <div className='bg-background w-full rounded-lg shadow-sm'>
-        <div className='p-4 border-b flex items-center justify-between'>
+        <div className='p-4 border-b flex gap-3 items-center justify-between'>
           <h2 className='text-lg font-extrabold'>All Products</h2>
-          <div className='flex items-center gap-2'>
+          <div className='flex items-center gap-3'>
             <span className='text-muted-[green]'>10 Products</span>
-          </div>
               <DropdownMenu>
-              <DropdownMenuTrigger asChild>
+                <DropdownMenuTrigger asChild>
                   <Button variant="outline" size="sm" className="flex items-center gap-1">
                     <LuArrowUpDown className='h-4 w-4'/>
                     <span>Sort by</span>
                   </Button>
-              </DropdownMenuTrigger>
-            </DropdownMenu>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className='w-[200px]'>
+                  <DropdownMenuRadioGroup>
+                    {
+                      sortOptions.map((sortItem) => (
+                        <DropdownMenuRadioItem key={sortItem.id}>
+                            {sortItem.label}
+                        </DropdownMenuRadioItem>
+                      ))
+                    }
+                  </DropdownMenuRadioGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+          </div>
         </div>
       </div>
     </div>
