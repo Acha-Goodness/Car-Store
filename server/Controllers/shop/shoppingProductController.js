5f4d1941247c414c8aebe883971be4f2d6878d56
Acha-Goodness <acharious1668@gmail.com>
2025-08-03 15:23:39 -0700

    filter product endpoint updated

--- server/Controllers/shop/shoppingProductController.js
+++ server/Controllers/shop/shoppingProductController.js
@@ -2,7 +2,11 @@ const AppError = require("../../Utils/appError");
 
 const getFilterProducts = async(req, res, next) => {
     try{
-
+        const products = await product.find({});
+        res.status(200).json({
+            success: true,
+            data: products
+        })
     }catch(e){
         console.log(e);
         return next(new AppError("Some error occured",500, res))
