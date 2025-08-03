4b5b14e59b65f33d95e7c1cfdf5b0a1bd434c00b
Acha-Goodness <acharious1668@gmail.com>
2025-08-03 14:31:01 -0700

    Shopping product tiles design

--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -35,6 +35,9 @@ const ShoppingListing = () => {
               </DropdownMenu>
           </div>
         </div>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4'>
+
+        </div>
       </div>
     </div>
   )
