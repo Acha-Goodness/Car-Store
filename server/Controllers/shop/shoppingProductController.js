895633dd896a8003fcd6ba14a5df31e3b4a4e0c3
Acha-Goodness <acharious1668@gmail.com>
2025-08-03 15:20:33 -0700

    filter error updated endpoint

--- server/Controllers/shop/shoppingProductController.js
+++ server/Controllers/shop/shoppingProductController.js
@@ -1,7 +1,10 @@
+const AppError = require("../../Utils/appError");
+
 const getFilterProducts = async(req, res, next) => {
     try{
 
     }catch(e){
         console.log(e);
+        return next(new AppError("Some error occured",500, res))
     }
 } 
