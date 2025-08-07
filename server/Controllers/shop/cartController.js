57f2dc0c877025b7eaf5d488c9241b3a02f96d4a
Acha-Goodness <acharious1668@gmail.com>
2025-08-07 10:54:33 -0700

    created add to cart endpoint

--- server/Controllers/shop/cartController.js
+++ server/Controllers/shop/cartController.js
@@ -6,7 +6,27 @@ const catchAsync = require("../../Utils/catchAsync");
 
 exports.addToCart = catchAsync(async (req, res, next) => {
     try{
-        
+        const { userId, productId, quantity } = req.body;
+
+        if(!userId || !productId || quantity <= 0){
+            return next(new AppError("Invalid data provided!", 400,res))
+        } 
+
+        const product = await Product.findById(productId);
+        if(!product) return next(new AppError("Product not found", 404, res));
+
+        let cart = Cart.findOne({ userId });
+        if(!cart) cart = new Cart({userId, items : []});
+
+        const findCurrentProductIndex = cart.items.findIndex(item => item.productId.toString() === productId);
+        if(findCurrentProductIndex === -1) cart.items.push({ productId, quantity})
+        else cart.items[findCurrentProductIndex].quantity += quantity;
+    
+        await cart.save();
+        res.status(200).json({
+            success: true,
+            data: cart
+        })
     }catch(err){
         console.log(err);
         return next(new AppError("Error", 500, res))
