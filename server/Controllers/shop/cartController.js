5292b73d9621788b949b94e0ca34e7b10b70263a
Acha-Goodness <acharious1668@gmail.com>
2025-08-07 11:41:36 -0700

    created update cart api logic

--- server/Controllers/shop/cartController.js
+++ server/Controllers/shop/cartController.js
@@ -52,11 +52,11 @@ exports.fetchCartItems = catchAsync(async (req, res, next) => {
         }
 
         const populateCartItems = validItems.map(item => ({
-            productId: item.productId,
-            image: item.image,
-            title: item.title,
-            price: item.price,
-            salePrice: item.salePrice,
+            productId: item.productId._id,
+            image: item.productId.image,
+            title: item.productId.title,
+            price: item.productId.price,
+            salePrice: item.productId.salePrice,
             quantity: item.quantity
         }))
 
@@ -76,7 +76,44 @@ exports.fetchCartItems = catchAsync(async (req, res, next) => {
 
 exports.updateCartItemQty = catchAsync(async (req, res, next) => {
     try{
+        const { userId, productId, quantity } = req.body;
 
+        if(!userId || !productId || quantity <= 0){
+            return next(new AppError("Invalid data provided!", 400,res))
+        } 
+
+        const cart = await Cart.findOne({userId})
+        if(!cart) return next(new AppError("Cart not found!", 404, res));
+
+        const findCurrentProductIndex = cart.items.findIndex(item => item.productId.toString() === productId);
+
+        if(findCurrentProductIndex === -1) return next(new AppError("Cart item not present", 404, res));
+
+        cart.items[findCurrentProductIndex].quantity = quantity;
+        await cart.save();
+
+        await cart.populate({
+            path: "items.productId",
+            select: "image title price salePrice"
+        })
+
+        const populateCartItems = cart.items.map(item => ({
+            productId: item.productId ? item.productId._id : null,
+            image: item.image ? item.productId.image : null,
+            title: item.title ? item.productId.title : "Product not found",
+            price: item.price ? item.productId.price : null,
+            salePrice: item.salePrice ? item.productId.salePrice : null,
+            quantity: item.quantity
+        }));
+
+        res.status(200).json({
+            success: true,
+            data: {
+                ...cart._doc,
+                items: populateCartItems,
+            }
+        })
+        
     }catch(err){
         console.log(err);
         return next(new AppError("Error", 500, res))
