d867bb29979d034eaee771d9face22e3981f9ff5
Acha-Goodness <acharious1668@gmail.com>
2025-08-08 04:58:27 -0700

    created asyncthunk for update cart quantity

--- client/src/store/shop/cart-slice/index.js
+++ client/src/store/shop/cart-slice/index.js
@@ -19,6 +19,15 @@ export const fetchCartItems = createAsyncThunk("/cart/fetchCartItems", async({us
     return response.data
 }) 
 
+export const updateCart = createAsyncThunk("/cart/updateCart", async({userId, productId, quantity}) => {
+    const response = await axios.put("http://localhost:3000/api/v1/shop/cart/update-cart", {
+        userId,
+        productId,
+        quantity
+    })
+    return response.data
+}) 
+
 export const deleteCartItems = createAsyncThunk("/cart/deleteCartItems", async({userId, productId}) => {
     const response = await axios.delete(`http://localhost:3000/api/v1/shop/cart/${userId}/${productId}`)
     return response.data
