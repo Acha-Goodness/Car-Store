ae7d82e86cc83ebbfa3820e2542a285f175186b1
Acha-Goodness <acharious1668@gmail.com>
2025-08-09 12:57:06 -0700

    implemented total cart amount function

--- client/src/components/shopping-view/cart-wrapper.jsx
+++ client/src/components/shopping-view/cart-wrapper.jsx
@@ -4,11 +4,17 @@ import { Button } from '../ui/button';
 import UserCartItemsContent from './cart-items-content';
 
 const UserCartWrapper = ({ cartItems }) => {
+
+  const totalAmt = cartItems && cartItems.length > 0 ?
+  cartItems.reduce((sum, currentItem) => sum + (
+    currentItem?.salePrice > 0 ? currentItem?.salePrice : currentItem?.price
+  ) * currentItem?.quantity, 0) : 0
+
   return (
     <SheetContent className="bg-white sm:max-w-md">
         <SheetHeader>
             <SheetTitle>Your Cart</SheetTitle>
-        </SheetHeader>
+        </SheetHeader>  
         <div className='mt-8 space-y-4'>
           {
             cartItems && cartItems.length > 0 &&
@@ -18,7 +24,7 @@ const UserCartWrapper = ({ cartItems }) => {
         <div className='mt-8 space-y-4'>
             <div className='flex justify-between'>
                 <span className='font-bold'>Total</span>
-                <span className='font-bold'>$1000</span>
+                <span className='font-bold'>${totalAmt}</span>
             </div>
         </div>
         <Button className="w-full mt-6 bg-black text-white">Checkout</Button>
